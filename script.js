7ef1e3327e6b2306f9b77dd333d095d0a853980c
Mohit Sharma <200160784+mohit716@users.noreply.github.com>
2026-05-26 23:40:36 -0400

    Wire HTML form to Express /submit endpoint

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const app = express();
+
+
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+    const { name, score } = req.body;
+    
+});
+
 
 
